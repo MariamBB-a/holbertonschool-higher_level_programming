985df1e7b113f9bb5ade1cae0e6ae158a85501ef
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-01-12 19:48:33 +0000

    fix the concatination

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School !"
-sentence = str1 + " " + str2
-print(f"Welcom to" (sentence))
+str1 = str1 + " " + str2
+print("Welcome to {}".format(str1))
