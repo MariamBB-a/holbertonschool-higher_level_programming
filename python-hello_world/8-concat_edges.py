709712ae020dca0e63e2e98fd3b53cdebb4cc298
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-01-12 20:43:01 +0000

    adding and printing new text with no new variable

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+text = "Python is an interpreted, interactive, object-oriented programming"
+ "language that combines remarkable power with very clear syntax"
+text = text + " " + "object-oriented programming with Python\n"
+print(text)
