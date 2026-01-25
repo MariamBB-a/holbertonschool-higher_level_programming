0355a587fd430a2341bc650be3561efb257fdf4c
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-01-25 17:19:30 +0000

    fixing floats

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -21,17 +21,14 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    # Reject NaN
-    if a != a:
+        try:
+        a = int(a)
+    except (ValueError, OverflowError):
         raise TypeError("a must be an integer")
-    if b != b:
+
+    try:
+        b = int(b)
+    except (ValueError, OverflowError):
         raise TypeError("b must be an integer")
 
-    # Reject infinity
-    if a in (float("inf"), float("-inf")):
-        raise TypeError("a must be an integer")
-    if b in (float("inf"), float("-inf")):
-        raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
-
+    return a + b
