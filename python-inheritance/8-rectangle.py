6ae20a390b1e472bfbc71385d0976dd8ef201f76
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-01-29 22:48:01 +0000

    pep8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module 8-rectangle: defines a
+Rectangle class inheriting from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle with width and height
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
