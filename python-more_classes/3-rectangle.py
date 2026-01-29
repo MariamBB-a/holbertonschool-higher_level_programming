ed314480f852a74ba03511e82ff3ecbcde90c4d7
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-01-29 22:00:29 +0000

    fix pep 8

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Module defines a Rectangle class with various utilities."""
+"""Module that defines a Rectangle class."""
 
 
 class Rectangle:
-    """Rectangle class with width, height, and utility methods."""
-
-    number_of_instances = 0
-    print_symbol = "#"
+    """Rectangle class with width, height, area, perimeter, and print."""
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle instance."""
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -53,33 +49,11 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Return the rectangle as a string using print_symbol."""
+        """Return the rectangle as a string using '#'."""
         if self.width == 0 or self.height == 0:
             return ""
-        symbol = str(self.print_symbol)
-        return "\n".join([symbol * self.width for _ in range(self.height)])
+        return "\n".join(["#" * self.width for _ in range(self.height)])
 
     def __repr__(self):
-        """Return a string to recreate the rectangle with eval()."""
-        return f"Rectangle({self.width}, {self.height})"
-
-    def __del__(self):
-        """Print a message when an instance is deleted."""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Return the rectangle with the bigger area (rect_1 if equal)."""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """Return a new Rectangle instance with width == height == size."""
-        return cls(size, size)
+        """Return the default representation (object address)."""
+        return super().__repr__()
