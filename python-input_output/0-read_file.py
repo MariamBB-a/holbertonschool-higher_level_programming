8e735b8614b6da0b5ad52408160d447e1cb16ba3
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-04 12:18:02 +0000

    correct stylle adding blank line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,7 @@ Module 0-read_file
 Contains a function that reads a text file and prints it to stdout.
 """
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
