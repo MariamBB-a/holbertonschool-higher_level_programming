3bbdfb6621130fb09eb4a6f711f4807fdf6eb567
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-04 12:21:30 +0000

    write a string

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
 Module 1-write_file
-Contains a function that writes a string to a text file (UTF8)
+Contains a function that writes a string to a  file (UTF8)
 and returns the number of characters written.
 """
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    """Writes a string to a file (UTF8) returns the n of characters ."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
 
