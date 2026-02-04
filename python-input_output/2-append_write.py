85e8beb923385fd147de530f0c28d31a16e9579f
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-04 12:34:30 +0000

    add file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,5 +7,6 @@ and returns the number of characters added.
 
 
 def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF8)"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
