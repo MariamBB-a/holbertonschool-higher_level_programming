3d4faaefa2602c526b70dcb5c051c35e993130ea
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-04 12:24:49 +0000

    delete blank line

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -10,4 +10,3 @@ def write_file(filename="", text=""):
     """Writes a string to a file (UTF8) returns the n of characters ."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-
