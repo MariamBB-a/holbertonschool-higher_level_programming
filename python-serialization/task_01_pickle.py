ac716fe0a1b517b941bac4ace339019b67a0ce65
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-05 09:13:31 +0000

    correcting pickel

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-""" sterializing costumers list"""
+"""CustomObject serialization using pickle"""
+
+import pickle
+
 
 class CustomObject:
-    """Class representing a costumer"""
+    """Class representing a custom object"""
+
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-    """display customer info"""
-        print(f"name: {self.name}")
+        """Display the object's attributes"""
+        print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
-    def sterialize(self, filename):
-    """sterialize current obj and save in file"""
-        with open(filename, 'wb') as f:
-            pickle.dump(self, f)
+    def serialize(self, filename):
+        """Serialize the object to a file"""
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except (OSError, pickle.PickleError):
+            return None
 
-    def destrialize(cls, filename):
-    """destrialize obj from file"""
-        with open(filename, 'rd') as f:
-            return pickle.load(f)
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize an object from a file"""
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except (OSError, pickle.PickleError):
+            return None
