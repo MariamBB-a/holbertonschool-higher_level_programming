631199e2f5e495aada03473edcecf091d309633c
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-05 08:19:48 +0000

    fixing indeintation and spelling json

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,12 +5,12 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-"""serialize python dic to json file"""
+    """serialize python dic to json file"""
     
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f)
 
 def load_and_deserialize(filename):
-"""deserialize data from the json file"""
-    with open(dilename, 'r', encoding='utf-8') as f:
-        return jason.load(f)
+    """deserialize data from the json file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
