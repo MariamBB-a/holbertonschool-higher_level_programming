faaeee4eaa3e4b0df8e7fd26b5e5bf481a6fe8ce
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-25 08:26:16 +0000

    in descending order for score record

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- lists all records where name is not NULL or empty, showing score and name, ordered by descending score
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
