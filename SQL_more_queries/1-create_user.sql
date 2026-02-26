3ed13e62bca645385d780e03e9ea4d331d571544
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-26 09:03:09 +0000

    create passward withut failling

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
--- creates user_0d_1 with passwords, without failing if it exists
+-- creates user_0d_1 with passwords
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
