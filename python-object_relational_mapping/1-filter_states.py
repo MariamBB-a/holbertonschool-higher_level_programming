583aad9333932fbf9455bc93ad93e65943ed336c
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-26 23:24:21 +0000

    listing all states starts with n

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with n"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    # Parameterized query to prevent SQL injection
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", ("N%",))
+
+    # Stream results row by row
+    for state in cursor:
+        print(state)
+
+    cursor.close()
+    db.close()
