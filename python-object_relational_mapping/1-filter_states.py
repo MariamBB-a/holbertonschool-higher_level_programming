2049cc87a80ab183c934dd88d72e76df1efcc4fb
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-26 23:52:35 +0000

    lowercase

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,21 +5,27 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connect to MySQL server
+    # Get MySQL login info from arguments
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=user,
+        passwd=passwd,
+        db=db_name
     )
+
     cursor = db.cursor()
+    # Use a case-insensitive LIKE for lowercase 'n'
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+    cursor.execute(query, ('n%',))  # lowercase 'n'
 
-    # Only lowercase 'n'
-    query = "SELECT * FROM states WHERE BINARY name LIKE %s ORDER BY id ASC"
-    cursor.execute(query)
-
-    for state in cursor:
+    # Print results
+    for state in cursor.fetchall():
         print(state)
 
     cursor.close()
