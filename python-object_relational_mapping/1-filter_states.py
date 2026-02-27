a3830a6d1ee9e52192c6e7fcd45ef708a05a638d
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-27 00:07:04 +0000

    lowercase

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # Execute SQL query
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
     # Fetch all rows and display results
     for row in cur.fetchall():
