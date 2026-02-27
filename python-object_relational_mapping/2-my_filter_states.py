33aa61e085e54336811004af9dbdd13c70eaae8d
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-02-27 00:18:29 +0000

    status in certain name arguments

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Lists states with a name matching the argument from the database"""
+"""Lists all states from the database hbtn_0e_0_usa
+where name matches the argument (safe against SQL injection)."""
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
 
-    # Create cursor object
+if __name__ == "__main__":
+    # Get arguments
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=passwd, db=db_name)
+
+    # Create cursor
     cur = db.cursor()
 
-    # Prepare SQL query using str.format with user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
-    )
-    cur.execute(query)
+    # Execute parameterized query
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
 
-    # Fetch all rows and display results
+    # Fetch and print results
     for row in cur.fetchall():
         print(row)
 
