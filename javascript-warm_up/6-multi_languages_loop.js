3ff472412aaec1b7509163bfbd1ae6fc9f33a4b5
MarBB <Mariam.backroush@gmail.com>
2026-03-28 15:24:50 +0300

    Create 6-multi_languages_loop.js

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = ['C', 'Python', 'JavaScript'];
+const yourVar = ['fun', 'cool', 'amazing'];
+
+for (let i = 0; i < myVar.length; i++) {
+  console.log(`${myVar[i]} is ${yourVar[i]}`);
+}
