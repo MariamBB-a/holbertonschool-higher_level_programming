7be3c97a4f4920ac1d536cd6c26c71d3105e45e5
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-03-28 12:45:47 +0000

    put

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-const yourVar = 'C is fun';
-const ourVar = 'Python is cool';
-console.log(yourVar);
-console.log(ourVar);
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
