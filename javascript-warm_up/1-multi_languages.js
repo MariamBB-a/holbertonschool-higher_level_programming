5d604e889033c2213d05173c565347e6e427b5cc
MarBB <Mariam.backroush@gmail.com>
2026-03-28 15:21:07 +0300

    Create 1-multi_languages.js

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myVar = 'JavaScript is amazing';
+const yourVar = 'C is fun';
+const ourVar = 'Python is cool';
+
+console.log(yourVar);
+console.log(ourVar);
+console.log(myVar);
