af30356149a89cdd5fd9ca2e6f3684e2ff15209a
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-03-30 15:37:05 +0000

    djv

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,33 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map(x => parseInt(x));
 
-const Arr = Number(process.argv.slice(2));
-
-function FindSecondLargest (a) {
-  if (a.length < 2) {
-    return 0;
-  }
-
-  if (isNaN(a)) {
-    return 0
-  }
-
-  let largest = a[0];
-  let secondLargest = -Infinity;
-
-  for (let i = 1; i < a.length; i++) {
-    if (a[i] > largest) {
-      secondLargest = largest;
-      largest = a[i];
-    } else if (a[i] > secondLargest && a[i] !== largest) {
-      secondLargest = a[i];
-    }
-  }
-
-  if (secondLargest === -Infinity) {
-    return 0;
-  } else {
-    return secondLargest;
-  }
+if (args.length < 2) {
+  console.log(0);
+} else {
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
 }
-
-console.log(FindSecondLargest(Arr));
