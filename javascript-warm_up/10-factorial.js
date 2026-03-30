2463cd777143acacaf33b481de8e2790370b40ea
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-03-30 15:34:18 +0000

    sn

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,18 +1,8 @@
 #!/usr/bin/node
-
-const x = Number(process.argv[2]);
-
-function factorial (a) {
-  if (a < 0) {
-    return 1;
-  }
-  if (a === 0) {
-    return 1;
-  }
-  if (isNaN(a)) {
-    return 1;
-  }
-  return (a * factorial(a - 1));
+function factorial (n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
 }
 
-console.log(factorial(x));
+const num = parseInt(process.argv[2]);
+console.log(factorial(isNaN(num) ? 1 : num));
